eb0fd4bfad9961a8f067c0d9e2d982b4e6895be6
min <rmdwjd76@naver.com>
2023-08-19 01:26:45 +0900

    Update notification.js

--- notification.js
+++ notification.js
@@ -1,6 +1,6 @@
 // 서비스 워커 등록
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js')
+  navigator.serviceWorker.register('./service-worker.js')
     .then(registration => {
       console.log('Service Worker registered with scope:', registration.scope);
     })
