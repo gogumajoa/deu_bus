8132a986d7cffa1cfabaf8befb6d781d433c867a
min <rmdwjd76@naver.com>
2023-08-19 03:54:56 +0900

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,11 @@ function initializePushNotification() {
         icon: 'clock.JPG', // 이미지 경로를 실제 이미지로 바꿔주세요.
       };
 
-      swRegistration.showNotification('웹 푸시 알림', options); // swRegistration 사용
+      if (swRegistration) {
+        swRegistration.showNotification('웹 푸시 알림', options); // swRegistration 사용
+      } else {
+        console.error('서비스 워커 등록 객체가 설정되지 않았습니다.');
+      }
     }, 1000); // 10초(10000 밀리초) 후에 실행
   });
 }
