0903224d4daa4d4172cb472fa4458596bc73d187
min <rmdwjd76@naver.com>
2023-08-19 01:44:19 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,16 @@
-self.addEventListener('install', event => {
-    console.log("Service worker Installing!");
-  });
-  
-  //service worker가 activate 되었을 때 이벤트 발생
-  self.addEventListener('activate', event => {
-    console.log("Service worker Activating!");
-    return self.clients.claim();
-  });
+// Register event listener for the 'push' event.
+self.addEventListener('push', function(event) {
+  // Retrieve the textual payload from event.data (a PushMessageData object).
+  // Other formats are supported (ArrayBuffer, Blob, JSON), check out the documentation
+  // on https://developer.mozilla.org/en-US/docs/Web/API/PushMessageData.
+  const payload = event.data ? event.data.text() : 'no payload';
+
+  // Keep the service worker alive until the notification is created.
+  event.waitUntil(
+    // Show a notification with title 'ServiceWorker Cookbook' and use the payload
+    // as the body.
+    self.registration.showNotification('ServiceWorker Cookbook', {
+      body: payload,
+    })
+  );
+});
