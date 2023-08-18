7a783d1a91842098a73e2443fadec0f7bc17fe80
min <rmdwjd76@naver.com>
2023-08-19 02:29:59 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,13 @@
+// sw.js
+
+self.addEventListener('push', function(event) {
+  const options = {
+    body: event.data.text(),
+    icon: 'icon.png', // 알림 아이콘 URL
+    badge: 'badge.png', // 뱃지 아이콘 URL
+  };
+
+  event.waitUntil(
+    self.registration.showNotification('웹 알림', options)
+  );
+});
